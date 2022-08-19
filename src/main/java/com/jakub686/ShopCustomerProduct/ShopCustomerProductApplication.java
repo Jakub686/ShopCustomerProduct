8cf293a791d0b3e8b897c05d761cd4a73c8c5522
Jakub <jakub.bchyla@gmail.com>
2022-08-19 15:55:17 +0200

    initial commit

---
+++ src/main/java/com/jakub686/ShopCustomerProduct/ShopCustomerProductApplication.java
@@ -0,0 +1,13 @@
+package com.jakub686.ShopCustomerProduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCustomerProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCustomerProductApplication.class, args);
+	}
+
+}
