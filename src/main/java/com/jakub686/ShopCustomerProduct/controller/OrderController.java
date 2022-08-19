0908839426402e87f67546416bf381d97a5a85fe
Jakub <jakub.bchyla@gmail.com>
2022-08-19 16:06:58 +0200

    added controller

---
+++ src/main/java/com/jakub686/ShopCustomerProduct/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.jakub686.ShopCustomerProduct.controller;
+
+import com.jakub686.ShopCustomerProduct.entity.Customer;
+import com.jakub686.ShopCustomerProduct.repository.CustomerRepository;
+import com.jakub686.ShopCustomerProduct.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class OrderController {
+    @Autowired
+    private CustomerRepository customerRepository;
+    @Autowired
+    private ProductRepository productRepository;
+
+    @PostMapping("/placeOrder")
+    public Customer placeOrder(@RequestBody OrderRequest request){
+        return customerRepository.save(request.getCustomer());
+    }
+
+    @GetMapping("/findAllOrders")
+    public List<Customer> findAllOrders(){
+        return customerRepository.findAll();
+    }
+
+    @GetMapping("/getInfo")
+    public List<OrderResponse> getJoinInformation(){
+        return customerRepository.getJoinInformation();
+    }
+}
